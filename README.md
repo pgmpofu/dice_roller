013fb9493088f77d7711ea5207f126d44e59d662
Gugulethu Mpofu <gugumpofu@Gugulethus-MacBook-Pro.local>
2020-11-20 23:37:23 -0800

    Added main activity content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dice_roller
+# Dice Roller
+
+A game that allows the user to guess a random number of dice through text input or speech and calculates the score.
---
+++ app/src/main/java/com/ai/diceroller/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ai.diceroller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        guessButton.setOnClickListener {
+            guess();
+        }
+    }
+
+    private fun guess() {
+        val drawableResource = when (Random().nextInt(6)+1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
