174efcc6a119ae2711ec4576c535534c12669943
techlord <pgmpofu@gmail.com>
2021-02-06 19:21:10 -0800

    Add ability to guess number via text

--- app/src/main/java/com/ai/diceroller/ui/GuessByVoice.kt
+++ app/src/main/java/com/ai/diceroller/ui/GuessByVoice.kt
@@ -2,11 +2,53 @@ package com.ai.diceroller.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
 import com.ai.diceroller.R
+import kotlinx.android.synthetic.main.activity_guess_by_text.*
+import kotlinx.android.synthetic.main.activity_guess_by_voice.*
+import kotlinx.android.synthetic.main.activity_guess_by_voice.diceImage
+import kotlinx.android.synthetic.main.activity_guess_by_voice.scoreValue
+import java.lang.NumberFormatException
+import java.util.*
 
-class GuessByVoice : AppCompatActivity() {
+class   GuessByVoice : AppCompatActivity() {
+
+    lateinit var guessText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guess_by_voice)
+
+        guessText = savedInstanceState?.get("mGuessText").toString()
+
+        guess(guessText)
+
+        doneButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun guess(guessText: String) {
+            val randomNumber = Random().nextInt(6) + 1
+            val drawableResource = when (randomNumber) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            diceImage.setImageResource(drawableResource)
+
+            if (randomNumber == guessText.toInt()) {
+                Toast.makeText(applicationContext, R.string.correct_guess, Toast.LENGTH_LONG).show()
+                scoreValue.text = Editable.Factory.getInstance().newEditable(
+                        scoreValue.text.toString()
+                                .toInt().plus(1).toString()
+                )
+            } else {
+                Toast.makeText(applicationContext, R.string.incorrect_guess, Toast.LENGTH_LONG).show()
+            }
     }
 }
--- app/src/main/java/com/ai/diceroller/ui/MainActivity.kt
+++ app/src/main/java/com/ai/diceroller/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ai.diceroller.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ai.diceroller.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,10 +20,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         microphoneImage.setOnClickListener{
-            var guessByVoice = Intent(this, GuessByVoice::class.java)
-            startActivity(guessByVoice)
+            val voiceIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            startActivityForResult(voiceIntent, REQUEST_CODE)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(resultCode){
+            RESULT_CANCELED -> Toast.makeText(this,"Could not catch that. Please try again", Toast.LENGTH_LONG).show()
+            RESULT_OK -> {
+                val stringArrayExtra = data?.getStringArrayExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0)
+                val guessByVoice = Intent(this, GuessByVoice::class.java)
+                        .putExtra("mGuessText", stringArrayExtra)
+                startActivity(guessByVoice)
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE = 0
+    }
 
 }
